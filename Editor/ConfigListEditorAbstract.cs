a4713176d13e4755aad0249d652d9a5905753761
Vasiliy <91281754+vasyasgors@users.noreply.github.com>
2026-03-13 22:50:11 +0300

    -

--- Editor/ConfigListEditorAbstract.cs
+++ Editor/ConfigListEditorAbstract.cs
@@ -3,67 +3,67 @@ using UnityEditor;
 
 using UnityEngine;
 
-namespace CodeBase.Infrastructure
-{ 
-    public abstract class ConfigListEditorAbstract : Editor
+
+
+public abstract class ConfigListEditorAbstract : Editor
+{
+    public override void OnInspectorGUI()
     {
-        public override void OnInspectorGUI()
+        base.DrawDefaultInspector();
+
+        SerializedProperty serializedProperty = serializedObject.FindProperty("Configs");
+
+        if (GUILayout.Button("Append Id") == true)
         {
-            base.DrawDefaultInspector();
 
-            SerializedProperty serializedProperty = serializedObject.FindProperty("Configs");
-
-            if (GUILayout.Button("Append Id") == true)
+            for(int i = 0; i < serializedProperty.arraySize; i++)
             {
+                Object obj = serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue;
 
-                for(int i = 0; i < serializedProperty.arraySize; i++)
+                if (obj == null) continue;
+
+                SerializedObject serializedObject = new SerializedObject(obj);
+
+
+                if (serializedObject.FindProperty("ID").intValue <= -1)
                 {
-                    Object obj = serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue;
-
-                    if (obj == null) continue;
-
-                    SerializedObject serializedObject = new SerializedObject(obj);
-
-
-                    if (serializedObject.FindProperty("ID").intValue <= -1)
-                    {
-                        serializedObject.FindProperty("ID").intValue = i;
-
-                        serializedObject.ApplyModifiedProperties();
-                        EditorUtility.SetDirty(serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue);
-                    }
-                }
-
-                AssetDatabase.SaveAssets();
-
-            }
-
-            if (GUILayout.Button("Full update id") == true)
-            {
-
-                for (int i = 0; i < serializedProperty.arraySize; i++)
-                {
-                    SerializedObject serializedObject = new SerializedObject(serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue);
-
                     serializedObject.FindProperty("ID").intValue = i;
 
                     serializedObject.ApplyModifiedProperties();
                     EditorUtility.SetDirty(serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue);
-                    
                 }
-
-                AssetDatabase.SaveAssets();
-
             }
-       
 
+            AssetDatabase.SaveAssets();
 
         }
 
+        if (GUILayout.Button("Full update id") == true)
+        {
+
+            for (int i = 0; i < serializedProperty.arraySize; i++)
+            {
+                SerializedObject serializedObject = new SerializedObject(serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue);
+
+                serializedObject.FindProperty("ID").intValue = i;
+
+                serializedObject.ApplyModifiedProperties();
+                EditorUtility.SetDirty(serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue);
+                    
+            }
+
+            AssetDatabase.SaveAssets();
+
+        }
+       
+
+
     }
 
-
 }
+
+
+
 
 
 
