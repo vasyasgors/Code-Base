ef1831718ade27f6b24246f4873ad3114c57baa3
Vasiliy <91281754+vasyasgors@users.noreply.github.com>
2026-08-21 01:42:07 +0300

    Update RuntimeStaticBatching.cs

--- Runtime/Stuff/RuntimeStaticBatching.cs
+++ Runtime/Stuff/RuntimeStaticBatching.cs
@@ -1,15 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace CodeBase
 {
     public class RuntimeStaticBatching : MonoBehaviour
     {
-        void Awake()
+
+        private void Awake()
         {
-            StaticBatchingUtility.Combine(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+
+            foreach (Transform child in transform)
+            {
+                GameObjectUtility.SetStaticEditorFlags(child.gameObject, StaticEditorFlags.BatchingStatic);
+            }
+
+            StaticBatchingUtility.Combine(gameObject);
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
 
     }
 }
